1bb26c9b5550115fb6b17326efd9f7a6054dfece
Thais Dordan <thais.dordan@liven.tech>
2021-04-17 13:47:16 -0300

    adicionando configuração do strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0c15e8aecd41619bdd51e4171df3f80'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a7f591a-d919-4982-bc25-a597caca77c5'
+};
